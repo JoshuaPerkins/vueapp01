f90adbac87709862283e37c6e829278754281e2d
Unknown <Perkins1220@hotmail.com>
2019-10-20 17:36:42 -0600

    Working sql postings on Post Manager page... need to incorporate other forms and such as wanted for final look then connect to sql.

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="md-layout">
+      <img :src="image"/>
+    </div>
+    <div class="md-layout">
+      <md-button>
+        <router-link to="/Auto">
+          {{ search }}
+        </router-link>
+      </md-button>
+      <md-button>
+        <router-link to="/Form">
+          Form
+        </router-link>
+      </md-button>
+      <md-button>
+        <router-link to="/Posts">
+          Posts
+        </router-link>
+      </md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainMenu",
+  data () {
+    return {
+        image: "https://images.pexels.com/photos/2071216/pexels-photo-2071216.jpeg?cs=srgb&dl=architectural-design-architecture-building-2071216.jpg&fm=jpg",
+        search: "Search-ing"
+    }
+  }
+};
+</script>
+
+<style scoped>
+.md-layout {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  height: 300px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import FormValidation from '@/components/FormValidation'
+import MainMenu from '@/components/MainMenu'
+import Autocomplete from '@/components/Autocomplete'
+import Posts from '@/components/PostsManager'
 
 Vue.use(Router)
 
@@ -8,8 +12,23 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'MainMenu',
+      component: MainMenu
+    },
+    {
+      path: '/Form',
+      name: 'FormValidation',
+      component: FormValidation
+    },
+    {
+      path: '/Auto',
+      name: 'Autocomplete',
+      component: Autocomplete
+    },
+    {
+      path: '/Posts',
+      name: 'Posts',
+      component: Posts
     }
   ]
 })
